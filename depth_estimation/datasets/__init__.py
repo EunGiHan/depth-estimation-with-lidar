59c76b3c40a6d0589a7ee377515c294efcef281f
EunGiHan <winterbloooom@gmail.com>
2022-07-09 12:56:11 +0000

    Black format change

--- depth_estimation/datasets/__init__.py
+++ depth_estimation/datasets/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .kitti import KITTIDataset
-from .custom import CustomDepthDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .custom import CustomDepthDataset
 from .kitti import KITTIDataset
 
 __all__ = [
